c3edba63fdcdb4679ec6f02159858a40766ed6d8
Guilherme <guilherme.ianhez2@gmail.com>
2017-11-07 15:27:02 -0200

    backend v2

---
+++ backend/app/controllers/schedule.py
@@ -0,0 +1,148 @@
+from flask import jsonify, request, session
+from app import app, db
+
+from app.models.tables import DayOfTheWeek, Laboratory, Profession, Permission, Schedule, SecurityKey, User
+from app.models.forms import LoginForm
+
+from app.controllers.functions import (SaveToDataBase, DeleteFromDataBase,
+ResponseBadRequest, ResponseCreated, ResponseGone, ResponseConflict,
+ResponseMethodNotAllowed, ResponseNotFound)
+from app.controllers.laboratory import SearchLaboratory
+import json
+import datetime
+
+
+@app.route("/schedule/create", methods=["POST"])
+def CreateSchedule():
+    #if not 'logged_in' in session: return ResponseUnauthorized()
+    if request.method != "POST": return ResponseMethodNotAllowed()
+    data = request.get_json()
+    if SearchSchedule(data_for_one=data): return ResponseConflict()
+    
+    if not CanCreateNewSchedule(data):
+        print("Conflito de horário")
+        return ResponseConflict()
+
+    lab = SearchLaboratory(ident=data['laboratory_id'])
+    if not lab: return ResponseNotFound()
+    new_schedule = CreateScheduleData(data, lab)
+    if not new_schedule: return ResponseBadRequest()
+    SaveToDataBase(new_schedule)
+    return ResponseCreated()
+
+
+@app.route("/schedule/read/<int:ident>", methods=["GET"])
+@app.route("/schedule/read", defaults={"ident": None}, methods=["GET"])
+def ReadSchedule(ident):
+    #if not 'logged_in' in session: return ResponseUnauthorized()
+    if request.method != "GET": return ResponseMethodNotAllowed()
+    output = []
+    schedules = []
+    if ident:
+        schedules.append(Schedule.query.filter_by(id=ident).first())
+    else:
+        schedules = Schedule.query.all()
+    
+    if schedules[0] == None: return ResponseNotFound()
+
+    for schedule in schedules:
+        schedule_data = {}
+        schedule_data['id'] = schedule.id
+        schedule_data['start'] = schedule.start.__str__()
+        schedule_data['end'] = schedule.end.__str__()
+        schedule_data['purpouse'] = schedule.purpouse
+        schedule_data['day_of_the_week'] = schedule.day_of_the_week.value
+        schedule_data['profession'] = schedule.profession.value
+        schedule_data['laboratory_id'] = schedule.laboratory_id
+        output.append(schedule_data)
+    
+    return jsonify({"schedules": output})
+
+
+@app.route("/schedule/update", methods=["PUT"])
+def UpdateSchedule():
+    from app.controllers.functions import ResponseOk
+    #if not 'logged_in' in session: return ResponseUnauthorized()
+    if request.method != "PUT": return ResponseMethodNotAllowed()
+
+    data = request.get_json()
+    schedule_update = SearchSchedule(ident=data["id"])
+    if not schedule_update: return ResponseNotFound()
+
+    if not CanUpdateSchedule(data): return ResponseConflict()
+    schedule_update.start = data['start']
+    schedule_update.end = data['end']
+    schedule_update.purpouse = data['purpouse']
+    schedule_update.day_of_the_week = data['day_of_the_week']
+    print("Valor do data[profession]: {0}\nTipo: {1}".format(data['profession'], type(data['profession'])))
+    schedule_update.profession = data['profession']
+    db.session.commit()
+    return ResponseOk()
+
+
+@app.route("/schedule/delete/<int:ident>", methods=["DELETE"])
+def DeleteSchedule(ident):
+    #if not 'logged_in' in session: return ResponseUnauthorized()
+    if request.method != "DELETE": return ResponseMethodNotAllowed()
+    if not ident: ResponseBadRequest()
+    schedule_delete = SearchSchedule(ident=ident)
+    if not schedule_delete: return ResponseNotFound()
+    DeleteFromDataBase(schedule_delete)
+    return ResponseGone()
+
+
+def CanCreateNewSchedule(data):
+    schedules = SearchSchedule(data_for_various=data)
+    for schedule in schedules:
+        if SubtractTime(schedule.start, data['start']) >= 0:
+            if ConvertToMinutes(schedule.start) < ConvertToMinutes(data['end']):
+                return False
+        else:
+            if ConvertToMinutes(schedule.end) > ConvertToMinutes(data['start']):
+                return False
+    return True
+
+
+def CanUpdateSchedule(data):
+    schedules = SearchSchedule(data_for_various=data)
+    current_schedule = SearchSchedule(ident=data['id'])
+    for schedule in schedules:
+        if schedule.id == current_schedule.id:
+            continue
+        if SubtractTime(schedule.start, data['start']) >= 0:
+            if ConvertToMinutes(schedule.start) < ConvertToMinutes(data['end']):
+                return False
+        else:
+            if ConvertToMinutes(schedule.end) > ConvertToMinutes(data['start']):
+                return False
+    return True
+
+
+def ConvertToMinutes(time):
+    if type(time) == datetime.time:
+        time = time.__str__()
+    time = time.split(':')
+    return int(time[0])*60 + int(time[1]) 
+
+
+def CreateScheduleData(data, lab):
+    return Schedule(start=data['start'], end=data['end'], purpouse=data['purpouse'],
+                    day_of_the_week=data['day_of_the_week'], profession=data['profession'],
+                    lab_agenda=lab)
+
+
+def SearchSchedule(data_for_one=None, ident=None, data_for_various=None):
+    if ident:
+        return Schedule.query.filter_by(id=ident).first()
+
+    elif data_for_one:
+        return Schedule.query.filter_by(start=data_for_one['start'], day_of_the_week=data_for_one['day_of_the_week'], laboratory_id=data_for_one['laboratory_id']).first()
+
+    elif data_for_various:
+        return Schedule.query.filter_by(laboratory_id=data_for_various['laboratory_id'], day_of_the_week=data_for_various['day_of_the_week']).all()
+
+
+def SubtractTime(time_a, time_b):
+    sum_a = ConvertToMinutes(time_a)
+    sum_b = ConvertToMinutes(time_b)
+    return sum_a-sum_b
---
+++ backend/run.py
@@ -0,0 +1,11 @@
+from app import manager
+from app import app
+
+
+if __name__ == "__main__":
+	#app.run(debug=True, host='127.0.0.1', port=5000)
+	app.run(debug=True, port=5000)
+	#host='192.168.43.128', port=4000, threaded=False
+	#manager.run()
+
+	#colocar o ip da VM no campo Host
